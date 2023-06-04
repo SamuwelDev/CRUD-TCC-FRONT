334ffbe31f6a49451012f55993fd0915e6d076ed
Loriano <lorianojr@hotmail.com>
2023-06-04 19:26:43 -0300

    Inclusão da Logo

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/crud-hotels'),
+      dir: require('path').join(__dirname, './coverage/SGH'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'crud-hotels'`, () => {
+  it(`should have as title 'SGH'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('crud-hotels');
+    expect(app.title).toEqual('SGH');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('crud-hotels app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('SGH app is running!');
   });
 });
